8fe851f4b3392f04c84c2286f0d5063c0310752d
RishiBuilds <rishi.chaurasia.dev@gmail.com>
2026-03-11 00:39:35 +0530

    refactor(auth): improve sign-in and sign-up page styling

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignIn />
+export default function SignInPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-muted/30">
+      <SignIn />
+    </div>
+  );
 }
